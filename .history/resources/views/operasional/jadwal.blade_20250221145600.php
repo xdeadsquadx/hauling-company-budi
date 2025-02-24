8120e78ad4c229c57254d23c7480ad823072cb1c
oamaebu <wkusuma544@gmail.com>
2025-02-24 12:06:43 +0700

    six commit

--- .history/resources/views/operasional/jadwal.blade_20250221145600.php
+++ .history/resources/views/operasional/jadwal.blade_20250221145600.php
@@ -2,7 +2,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
-    <link rel="stylesheet" href="{{ asset('/build/assets/app.css') }}">  
+    <link rel="stylesheet" href="{{ asset('/build/assets/app-CBpTEU8c.css') }}">
+<link rel="stylesheet" href="{{ asset('/build/assets/app-DnVUw_vK.css') }}">  
 </head>
 <body class="">
     @include('component.sidebar')
