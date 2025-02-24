43ef0e69ab184ce66b7711b3be53953be7e82422
oamaebu <wkusuma544@gmail.com>
2025-02-24 07:51:56 +0700

    fifth commit

--- .history/vite.config_20250224074919.js
+++ .history/vite.config_20250224074919.js
@@ -6,7 +6,8 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-        })      
+        }),
+        
     ],
     base: "/build/",
 });
